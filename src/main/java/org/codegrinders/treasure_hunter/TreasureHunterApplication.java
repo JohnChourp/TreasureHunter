b42dfbd8ce7fd666e9e5941ef2b80e0b7bb7e7e3
FanisCham <O4Nwu2vr>
2020-11-09 14:56:17 +0200

    added profileLogin method

--- src/main/java/org/codegrinders/treasure_hunter/TreasureHunterApplication.java
+++ src/main/java/org/codegrinders/treasure_hunter/TreasureHunterApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TreasureHunterApplication {
+    private static String savedUsername="user1";
+    private static String savedPassword="1234567890";
 
     public static void main(String[] args) {
         SpringApplication.run(TreasureHunterApplication.class, args);
+    }
+    public static void profileLogin(String username, String password){
+        if(username.equals(savedUsername) && password.equals(savedPassword))
+            System.out.println("You have successfully logged in to the game");
+        else
+            System.out.println("Wrong username or password");
     }
 
 }
